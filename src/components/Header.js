20c5b07085aa8c5f8b3252731cebde02d90d4ea2
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-08-29 13:20:47 +0300

    Style Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,75 @@
 import React from "react";
+import styled from "styled-components/macro";
+
+import Hamburger from "./Hamburger";
 import Navbar from "./Navbar";
-import HeaderCTA from "./HeaderCTA";
+import heroImageDesktop from "../images/desktop/image-header.jpg";
+import heroImageMobile from "../images/mobile/image-header.jpg";
+import { ReactComponent as SunnysideLogo } from "../images/logo.svg";
+import arrowDownSVG from "../images/icon-arrow-down.svg";
+
+const StyledHeadline = styled.h1`
+  font-family: var(--ff-secondary);
+  font-weight: 900;
+  font-size: 2.5em;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  line-height: 1.2em;
+  color: var(--white);
+  text-align: center;
+  margin-top: 2.5em;
+`;
+
+const StyledPageHeader = styled.header`
+  padding: 1em;
+  background-image: url(${heroImageMobile});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: bottom center;
+  height: 90vh;
+  position: relative;
+
+  @media (min-width: 430px) {
+    background-position: center 70%;
+  }
+
+  @media (min-width: 480px) {
+    background-position: center 80%;
+  }
+
+  @media (min-width: 540px) {
+    background-position: center 50%;
+  }
+
+  @media (min-width: 560px) {
+    background-image: url(${heroImageDesktop});
+  }
+`;
+
+const StyledTopBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledArrowDown = styled.img`
+  display: block;
+  margin: 3em auto;
+  height: 7em;
+  width: 2em;
+`;
 
 function Header() {
   return (
-    <header>
-      <h1>Header Component</h1>
-      Logo
-      <Navbar />
-      <HeaderCTA />
-      We are creatives
-    </header>
+    <StyledPageHeader>
+      <StyledTopBar>
+        <SunnysideLogo />
+        <Hamburger />
+        <Navbar />
+      </StyledTopBar>
+      <StyledHeadline>We are creatives</StyledHeadline>
+      <StyledArrowDown src={arrowDownSVG} alt="arrow down icon" />
+    </StyledPageHeader>
   );
 }
 
