8fe317de51f4de72ecfe1849acbd827ffc984faf
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-08-29 13:20:06 +0300

    Add Hamburger component

---
+++ src/components/Hamburger.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import hamburgerSVG from "../images/icon-hamburger.svg";
+
+const StyledHamburgerSvg = styled.img`
+  width: 1.5em;
+  height: 1em;
+  cursor: pointer;
+
+  &:hover,
+  &:active {
+    width: 1.55em;
+    height: 1.05em;
+  }
+
+  @media (min-width: 650px) {
+    display: none;
+  }
+`;
+
+function Hamburger() {
+  return <StyledHamburgerSvg src={hamburgerSVG} alt="hamburger icon" />;
+}
+
+export default Hamburger;
