b757f00002fff36c9c184c80cc26fd941200b417
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-08-30 04:13:07 +0300

    Refactor Main component to use JSON data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,23 @@
 import React from "react";
+import styled from "styled-components/macro";
+
+import Jumbotron from "./Jumbotron";
+import ImageCard from "./ImageCard";
+import jumbotronsData from "../fixtures/jumbotronsData";
+import imageCardsData from "../fixtures/imageCardsData";
+
+const StyledMain = styled.main``;
 
 function Main() {
   return (
-    <main>
-      <section>
-        <h2>Transform your brand</h2>
-        We are a full-service creative agency specializing in helping brands
-        grow fast. Engage your clients through compelling visuals that do most
-        of the marketing for you. Learn more
-      </section>
-      <section>
-        <h2>Stand out to the right audience</h2>
-        Using a collaborative formula of designers, researchers, photographers,
-        videographers, and copywriters, we’ll build and extend your brand in
-        digital places. Learn more
-      </section>
-      <section>
-        <div>
-          <h2>Graphic design</h2>
-          Great design makes you memorable. We deliver artwork that underscores
-          your brand message and captures potential clients’ attention.
-        </div>
-        <div>
-          <h2>Photography</h2>
-          Increase your credibility by getting the most stunning, high-quality
-          photos that improve your business image.
-        </div>
-      </section>
-    </main>
+    <StyledMain>
+      {jumbotronsData.map((data) => (
+        <Jumbotron {...data} key={data.id} />
+      ))}
+      {imageCardsData.map((data) => (
+        <ImageCard {...data} key={data.id} />
+      ))}
+    </StyledMain>
   );
 }
 
