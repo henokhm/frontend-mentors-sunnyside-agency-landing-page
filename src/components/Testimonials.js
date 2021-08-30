26aaad68d4f92b06d019c542d4d2358df3726939
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-08-30 15:28:53 +0300

    Extract testimoialsData to json file

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,23 +1,6 @@
 import React from "react";
 import Testimonial from "./Testimonial";
-
-const testimonialsData = [
-  {
-    author: "Emily R.",
-    position: "Marketing Director",
-    body: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
-  },
-  {
-    author: "Thomas S.",
-    position: "Chief Operating Officer",
-    body: "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
-  },
-  {
-    author: "Jennie F.",
-    position: "Business Owner",
-    body: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
-  },
-];
+import testimonialsData from "../fixtures/testimonialsData.json";
 
 function Testimonials() {
   return (
