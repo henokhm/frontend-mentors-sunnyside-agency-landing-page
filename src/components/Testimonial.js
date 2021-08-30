1fe69b2a7a329f95281878100bb54e825dfe6245
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-08-30 16:02:13 +0300

    Add Testimonial styles

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,12 +1,68 @@
 import React from "react";
+import styled from "styled-components/macro";
 
-function Testimonial({ author, position, body }) {
+const StyledTestimonial = styled.div`
+  padding: 1.5em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 450px;
+  margin: 0 auto;
+
+  & img {
+    border-radius: 50%;
+    width: 50%;
+    padding: 1.5em;
+    max-width: 200px;
+  }
+
+  & blockquote {
+    text-align: center;
+    color: var(--very-dark-grayish-blue);
+    line-height: 1.4em;
+  }
+
+  &.testimonial__meta {
+    display: flex;
+    flex-direction: column;
+    padding: 2.5em;
+  }
+
+  &.testimonial__author {
+    font-family: "Fraunces", serif;
+    color: var(--very-dark-desaturated-blue);
+    padding-bottom: 0.5em;
+  }
+
+  &.testimonial__position {
+    font-size: 0.75em;
+    color: var(--grayish-blue);
+  }
+
+  @media (min-width: 850px) {
+    width: 50%;
+  }
+
+  @media (min-width: 1100px) {
+    width: 33%;
+  }
+`;
+
+function Testimonial({ author, position, body, photoURL }) {
   return (
-    <div>
-      <h3>{author}</h3>
-      <p>{position}</p>
-      <p>{body}</p>
-    </div>
+    <StyledTestimonial>
+      <img src={`${process.env.PUBLIC_URL}${photoURL}`} alt={author} />
+      <blockquote>
+        <p>{body}</p>
+        <footer>
+          <div class="testimonial__meta">
+            <cite class="testimonial__author">{author}</cite>
+            <span class="testimonial__position">{position}</span>
+          </div>
+        </footer>
+      </blockquote>
+    </StyledTestimonial>
   );
 }
 
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,15 +1,42 @@
 import React from "react";
+import styled from "styled-components/macro";
+
 import Testimonial from "./Testimonial";
 import testimonialsData from "../fixtures/testimonialsData.json";
 
+const StyledTestimonials = styled.section`
+  & h2 {
+    font-family: "Fraunces", serif;
+    font-size: 1.25em;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    padding: 3em 1em;
+    color: var(--grayish-blue);
+    text-align: center;
+  }
+`;
+
+const StyledTestimonialsWrapper = styled.div`
+  @media (min-width: 850px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  @media (min-width: 950px) {
+  }
+`;
+
 function Testimonials() {
   return (
-    <section>
+    <StyledTestimonials>
       <h2>Client testimonials </h2>
-      {testimonialsData.map((testimonial) => (
-        <Testimonial {...testimonial} key={testimonial.author} />
-      ))}
-    </section>
+      <StyledTestimonialsWrapper>
+        {testimonialsData.map((testimonial) => (
+          <Testimonial {...testimonial} key={testimonial.author} />
+        ))}
+      </StyledTestimonialsWrapper>
+    </StyledTestimonials>
   );
 }
 
