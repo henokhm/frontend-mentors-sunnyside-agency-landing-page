30d461237c466426a6d90ea92a15a67e6c9d4e39
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-08-30 16:26:34 +0300

    Add ImageGallary styles and json

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,10 +1,39 @@
 import React from "react";
+import styled from "styled-components/macro";
+
+import imageGallaryData from "../fixtures/imageGallaryData.json";
+
+const StyledImageGallary = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+
+  & img {
+    width: 50%;
+    margin: 0;
+    padding: 0;
+  }
+
+  @media (min-width: 950px) {
+    & img {
+      width: 25%;
+    }
+  }
+`;
 
 function ImageGallery() {
   return (
-    <section>
-      <h3>Image Gallary!</h3>
-    </section>
+    <StyledImageGallary>
+      {imageGallaryData.map((data) => (
+        <img
+          src={`${process.env.PUBLIC_URL}${data.imageURLDesktop}`}
+          srcset={`
+              ${`${process.env.PUBLIC_URL}${data.imageURLMobile}`}  651w,
+              ${`${process.env.PUBLIC_URL}${data.imageURLDesktop}`} 1440w
+            `}
+          alt={data.imageAlt}
+        />
+      ))}
+    </StyledImageGallary>
   );
 }
 
